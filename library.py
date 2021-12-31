1b0496f95cfc380a19b9b6f9dab15181252455bb
TheNotoriousMMA <f74084046@gs.ncku.edu.tw>
2022-01-01 00:13:09 +0800

    Update

---
+++ library.py
@@ -0,0 +1,24 @@
+import os
+import re
+import sys
+import requests
+import webbrowser
+import message_template
+
+from bs4 import BeautifulSoup
+from transitions.extensions import GraphMachine
+from lxml import etree
+from PIL import Image
+from pyquery import PyQuery
+from flask import *
+from dotenv import load_dotenv
+from linebot import *
+from linebot.exceptions import *
+from linebot.models import *
+from selenium import webdriver
+from selenium.webdriver.support import ui
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+from fsm import TocMachine
+from utils import *
---
+++ message_template.py
@@ -0,0 +1,327 @@
+feature = {
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    },
+    "offsetTop": "xxl"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "OPTIONS",
+        "weight": "bold",
+        "size": "3xl",
+        "align": "center",
+        "offsetEnd": "none",
+        "offsetStart": "none",
+        "offsetBottom": "md"
+      },
+      {
+        "type": "separator"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "依照階級查詢",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Search by tier",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "依照英雄查詢",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Search by champion",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "依照對抗查詢",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Search by matchup",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      }
+    ],
+    "offsetBottom": "none"
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "spacer",
+        "size": "sm"
+      }
+    ],
+    "flex": 0
+  }
+}
+#============================================================================================
+service = {
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    },
+    "offsetTop": "xxl"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "OPTIONS",
+        "weight": "bold",
+        "size": "3xl",
+        "align": "center",
+        "offsetEnd": "none",
+        "offsetStart": "none",
+        "offsetBottom": "md"
+      },
+      {
+        "type": "separator"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "下載圖片",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Download Image",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "英雄數據",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Champion Statistics",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "英雄故事",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Champion Story",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      }
+    ],
+    "offsetBottom": "none"
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "spacer",
+        "size": "sm"
+      }
+    ],
+    "flex": 0
+  }
+}
+#============================================================================================
+info = {
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    },
+    "offsetTop": "xxl"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "OPTIONS",
+        "weight": "bold",
+        "size": "3xl",
+        "align": "center",
+        "offsetEnd": "none",
+        "offsetStart": "none",
+        "offsetBottom": "md"
+      },
+      {
+        "type": "separator"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "勝率",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Win rate",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "text",
+            "text": "選取率",
+            "color": "#aaaaaa",
+            "size": "sm",
+            "flex": 3
+          },
+          {
+            "type": "text",
+            "text": "Pick rate",
+            "wrap": True,
+            "color": "#666666",
+            "size": "sm",
+            "flex": 5
+          }
+        ],
+        "margin": "sm"
+      }
+    ],
+    "offsetBottom": "none"
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "spacer",
+        "size": "sm"
+      }
+    ],
+    "flex": 0
+  }
+}
